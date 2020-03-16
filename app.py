dbc1c7848082213f6c9b5d7cd6147d5aa7418220
Igor Basuga <igor.basuga@gmail.com>
2020-03-16 10:02:36 +0000

    create folder structure; add env.py, .gitignore and requirements

---
+++ app.py
@@ -0,0 +1,24 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for, session, flash
+from flask_pymongo import PyMongo
+from bson.object import ObjectId
+if os.path.exists("env.py"):
+    import env
+
+app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = "task_manager"
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
+mongo = PyMongo(app)
+
+
+@app.route("/")
+def index():
+    return "<h1>Hello Flask!</h1>"
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
