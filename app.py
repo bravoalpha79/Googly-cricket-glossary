202bf1488425561d36236c07037902a4c2beb90d
Igor Basuga <igor.basuga@gmail.com>
2020-03-16 10:07:49 +0000

    fix typo in app.py imports

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for, session, flash
 from flask_pymongo import PyMongo
-from bson.object import ObjectId
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
