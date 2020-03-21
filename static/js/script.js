58758ebd267a1c637741c5863385a0c0c9ad9eb9
Igor Basuga <igor.basuga@gmail.com>
2020-03-21 11:34:53 +0000

    add Materialize classes to form fields; modify addMeaning() accordingly

--- static/js/script.js
+++ static/js/script.js
@@ -7,13 +7,22 @@ $(document).ready(function(){
 function addMeaning() {
     let meaningList = document.getElementsByClassName("meaning");
     let newMeaningNum = meaningList.length + 1;
-    let newMeaning = document.createElement("textarea");
-        
-    newMeaning.id = `meaning${newMeaningNum}`;
-    newMeaning.setAttribute("class", "meaning");
-    newMeaning.setAttribute("name", `meaning${newMeaningNum}`);
-    newMeaning.setAttribute("placeholder", `meaning ${newMeaningNum}`);
-    newMeaning.setAttribute("cols", "40");
 
-    document.getElementById("meanings").appendChild(newMeaning);
+    let newMeaningRow = document.createElement("div");
+    let newMeaningWrapper = document.createElement("div");
+    let newMeaningTextarea = document.createElement("textarea");
+    
+    newMeaningRow.setAttribute("class", "row");
+
+    newMeaningWrapper.setAttribute("class", "input-field col s8")
+
+    newMeaningTextarea.id = `meaning${newMeaningNum}`;
+    newMeaningTextarea.setAttribute("class", "meaning materialize-textarea");
+    newMeaningTextarea.setAttribute("name", `meaning${newMeaningNum}`);
+    newMeaningTextarea.setAttribute("placeholder", `meaning ${newMeaningNum}`);
+    
+    newMeaningWrapper.appendChild(newMeaningTextarea);
+    newMeaningRow.appendChild(newMeaningWrapper);
+
+    document.getElementById("meanings").appendChild(newMeaningRow);
 }
