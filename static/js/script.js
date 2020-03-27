41b57908108d7f8c8a2bec1a453c25fe85f0af54
Igor Basuga <igor.basuga@gmail.com>
2020-03-27 20:02:18 +0000

    add side navigation for large screens; add search box

--- static/js/script.js
+++ static/js/script.js
@@ -9,66 +9,66 @@ $(document).ready(function(){
 //dotenv initialisation
 require('dotenv').config()
 
-/**
- * AutoComplete search form.
- * Application code obtained from https://tarekraafat.github.io/autoComplete.js/#/
- * and adapted to project needs.  
- */
-new autoComplete({
-    data: {                              // Data src [Array, Function, Async] | (REQUIRED)
-      src: async () => {
-        // API key token
-        const token = process.env.MONGO_URI;
-        // User search query
-        const query = document.querySelector("#autoComplete").value;
-        // Fetch External Data Source
-        const source = await fetch(`https://cloud.mongodb.com/api/atlas/v1.0/search?key=${token}&q=${query}`);
-        // Format data into JSON
-        const data = await source.json();
-        // Return Fetched data
-        return data.entries;
-      },
-      key: ["form"],
-      cache: false
-    },
-    sort: (a, b) => {                    // Sort rendered results ascendingly | (Optional)
-        if (a.match < b.match) return -1;
-        if (a.match > b.match) return 1;
-        return 0;
-    },
-    placeHolder: "Enter word...",     // Place Holder text                 | (Optional)
-    selector: "#autoComplete",           // Input field selector              | (Optional)
-    threshold: 3,                        // Min. Chars length to start Engine | (Optional)
-    debounce: 300,                       // Post duration for engine to start | (Optional)
-    searchEngine: "strict",              // Search Engine type/mode           | (Optional)
-    resultsList: {                       // Rendered results list object      | (Optional)
-        render: true,
-        container: source => {
-            source.setAttribute("id", "word_list");
-        },
-        destination: document.querySelector("#autoComplete"),
-        position: "afterend",
-        element: "ul"
-    },
-    maxResults: 7,                         // Max. number of rendered results | (Optional)
-    highlight: true,                       // Highlight matching results      | (Optional)
-    resultItem: {                          // Rendered result item            | (Optional)
-        content: (data, source) => {
-            source.innerHTML = data.match;
-        },
-        element: "li"
-    },
-    noResults: () => {                     // Action script on noResults      | (Optional)
-        const result = document.createElement("li");
-        result.setAttribute("class", "no_result");
-        result.setAttribute("tabindex", "1");
-        result.innerHTML = "No Results";
-        document.querySelector("#autoComplete_list").appendChild(result);
-    },
-    onSelection: feedback => {             // Action script onSelection event | (Optional)
-        console.log(feedback.selection.value.image_url);
-    }
-});
+// /**
+//  * AutoComplete search form.
+//  * Application code obtained from https://tarekraafat.github.io/autoComplete.js/#/
+//  * and adapted to project needs.  
+//  */
+// new autoComplete({
+//     data: {                              // Data src [Array, Function, Async] | (REQUIRED)
+//       src: async () => {
+//         // API key token
+//         const token = process.env.MONGO_URI;
+//         // User search query
+//         const query = document.querySelector("#autoComplete").value;
+//         // Fetch External Data Source
+//         const source = await fetch(`https://cloud.mongodb.com/api/atlas/v1.0/search?key=${token}&q=${query}`);
+//         // Format data into JSON
+//         const data = await source.json();
+//         // Return Fetched data
+//         return data.entries;
+//       },
+//       key: ["form"],
+//       cache: false
+//     },
+//     sort: (a, b) => {                    // Sort rendered results ascendingly | (Optional)
+//         if (a.match < b.match) return -1;
+//         if (a.match > b.match) return 1;
+//         return 0;
+//     },
+//     placeHolder: "Enter word...",     // Place Holder text                 | (Optional)
+//     selector: "#autoComplete",           // Input field selector              | (Optional)
+//     threshold: 3,                        // Min. Chars length to start Engine | (Optional)
+//     debounce: 300,                       // Post duration for engine to start | (Optional)
+//     searchEngine: "strict",              // Search Engine type/mode           | (Optional)
+//     resultsList: {                       // Rendered results list object      | (Optional)
+//         render: true,
+//         container: source => {
+//             source.setAttribute("id", "word_list");
+//         },
+//         destination: document.querySelector("#autoComplete"),
+//         position: "afterend",
+//         element: "ul"
+//     },
+//     maxResults: 7,                         // Max. number of rendered results | (Optional)
+//     highlight: true,                       // Highlight matching results      | (Optional)
+//     resultItem: {                          // Rendered result item            | (Optional)
+//         content: (data, source) => {
+//             source.innerHTML = data.match;
+//         },
+//         element: "li"
+//     },
+//     noResults: () => {                     // Action script on noResults      | (Optional)
+//         const result = document.createElement("li");
+//         result.setAttribute("class", "no_result");
+//         result.setAttribute("tabindex", "1");
+//         result.innerHTML = "No Results";
+//         document.querySelector("#autoComplete_list").appendChild(result);
+//     },
+//     onSelection: feedback => {             // Action script onSelection event | (Optional)
+//         console.log(feedback.selection.value.image_url);
+//     }
+// });
 
 
 
