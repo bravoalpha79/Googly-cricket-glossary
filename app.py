5581a1fa723f8cf62e58961a2e48c8f55d64c021
Igor Basuga <igor.basuga@gmail.com>
2020-03-30 08:40:34 +0000

    add docstrings and comments into app.py and clean up code

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for, session, flash
-from flask import jsonify
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
@@ -14,7 +13,11 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
+# constants created to avoid repetition in code
+
+# constant for letter navigation
 ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 DB = mongo.db
 
 
@@ -25,22 +28,25 @@ def index():
 
 @app.route("/display_letter/<letter>")
 def display_letter(letter):
+    """Display links to all words starting with selected letter."""
     return render_template("letter.html",
                            letter=DB.entries.find({
-                           "letter": letter}).sort("term"), 
+                            "letter": letter}).sort("term"),
                            letters=ALPHABET)
 
 
 @app.route("/find_words", methods=["GET", "POST"])
 def find_words():
+    """Find and display word(s) matching a searchbox query."""
     entries = DB.entries
     all_entries = entries.find()
+    # list comprehension method obtained from datacamp.com
     all_words = [item["term"] for item in all_entries]
 
     search_term = request.form["search"]
 
     matches = []
-    
+
     for entry in all_words:
         if search_term and entry[0:len(search_term)] == search_term:
             matches.append(entry)
@@ -51,6 +57,7 @@ def find_words():
 
 @app.route("/display_word/<word>")
 def display_word(word):
+    """Display full entry details of selected word."""
     return render_template("word.html",
                            word=DB.entries.find_one({
                             "term": word}), letters=ALPHABET)
@@ -58,11 +65,13 @@ def display_word(word):
 
 @app.route("/add_word")
 def add_word():
+    """Display form for adding new word."""
     return render_template("addword.html", letters=ALPHABET)
 
 
 @app.route("/insert_word", methods=["GET", "POST"])
 def insert_word():
+    """Process addword form and insert word into DB."""
     word = request.form["term"].lower()
     entries = DB.entries
     all_entries = DB.entries.find()
@@ -70,12 +79,12 @@ def insert_word():
     glossary = [item.lower() for item in all_words]
 
     meanings = []
-
+    # key-value iteration code obtained from W3Schools.com
     for k, v in request.form.items():
         if k != "term":
             if v != "":
                 meanings.append(v)
-
+    # check if same word already exists in DB
     if word in glossary:
         flash(("Entry '{}' already exists.").format(word))
         return redirect(url_for("add_word"))
@@ -92,6 +101,7 @@ def insert_word():
 
 @app.route("/edit_word/<word_id>")
 def edit_word(word_id):
+    """ Display editword form for selected word"""
     word_to_edit = DB.entries.find_one({"_id": ObjectId(word_id)})
     return render_template("editword.html",
                            word=word_to_edit, letters=ALPHABET)
@@ -99,17 +109,18 @@ def edit_word(word_id):
 
 @app.route("/update_word/<word_id>", methods=["GET", "POST"])
 def update_word(word_id):
+    """Process editword form and insert update(s) into DB"""
     entries = DB.entries
     term_to_update = request.form["term"].lower()
     same_term = entries.find_one({"term": term_to_update})
-    
-    meanings = []
 
+    meanings = []
+    # key-value iteration code obtained from W3Schools.com
     for k, v in request.form.items():
         if k != "term":
             if v != "":
                 meanings.append(v)
-
+    # if word (term) is modified, check if same entry already exists in DB
     if same_term and same_term["_id"] != ObjectId(word_id):
         flash(("Entry '{}' already exists.").format(term_to_update))
         return redirect(url_for("edit_word",
@@ -127,6 +138,7 @@ def update_word(word_id):
 
 @app.route("/delete_word/<word_id>")
 def delete_word(word_id):
+    """Delete selected entry from DB."""
     entries = DB.entries
     entries.delete_one({"_id": ObjectId(word_id)})
     flash("Entry successfully deleted.")
@@ -140,13 +152,16 @@ def contribute():
 
 @app.route("/authenticate_user", methods=["GET", "POST"])
 def authenticate_user():
+    """Process contributor authorisation form."""
     contributors = DB.contributors.find()
     passwords = [entry["passkey"] for entry in contributors]
 
     if request.method == "POST":
+        # check if contributor is already logged in
         if "username" in session:
             flash("You are already authorised as contributor")
             return render_template("contribute.html", letters=ALPHABET)
+        # check if password exists in DB
         elif request.form["contributor"] in passwords:
             session["username"] = "contributor"
             flash("Contributor authorisation successful.")
@@ -158,6 +173,8 @@ def authenticate_user():
 
 @app.route("/logout")
 def logout():
+    """Log out contributor."""
+    # session.pop method obtained from Stack Overflow
     session.pop("username")
     flash("Successfully logged out.")
     return render_template("index.html", letters=ALPHABET)
